dc53960c9c0579f79df9bb1338f5696394caa242
Kömarov <omar.carrasco@usach.cl>
2018-05-07 02:50:29 -0300

    Funciones de Pertenencia.
    y otros.

--- BC - ChatBot.pl
+++ BC - ChatBot.pl
@@ -1,3 +1,23 @@
 %Hechos
+%Constructor:
+cb([]).
+seed(0).
+seed(1).
 
 %Reglas
+%Pertenencia (Log):
+esLog([]).
+esLog([X|Xs]):-
+     string(X).
+%Pertenencia (Chatbot):
+esChatbot([]).
+esChatbot([X|Xs]):-
+     (X = 0;X = 1).
+
+% Agregar elemento ELEM a una lista
+agregar([], [ELEM], ELEM).
+agregar([X|Xs], [X|Ys], ELEM):-
+     agregar(Xs, Ys, ELEM).
+
+beginDialog(Chatbot, InputLog, Seed, OutputLog):-
+     agregar(InputLog, OutputLog, "Bienvenido! :D!").
