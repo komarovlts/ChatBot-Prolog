4edcfceaf3a9de0ff9da506473dccbb896317961
Kömarov <omar.carrasco@usach.cl>
2018-05-13 03:12:40 -0400

    Atom to String.
    Se ha cambiado todas las aplicaciones de atom_concat a string_concat.
    (no funcionaba por un error de concepto, el cual ya ha sido solucionado)

--- BC - ChatBot.pl
+++ BC - ChatBot.pl
@@ -26,33 +26,40 @@ agregar([X|Xs], [X|Ys], ELEM):-
 beginDialog(Chatbot, InputLog, Seed, OutputLog):-
      get_time(T), stamp_date_time(T, date(_, _, _, Hour, _, _, _, _, _), 'local'),
      horaYfechaActual(StringTime),
-     agregar(InputLog, NuevoInputLog, 'BeginDialog'),
-     inicioConversacion(Chatbot, NuevoInputLog, Hour, StringTime, OutputLog).
+     agregar(InputLog, NuevoInputLog, "|BeginDialog|"),
+     inicioConversacion(Chatbot, NuevoInputLog, Hour, StringTime, PrevioOutputLog),
+     agregar(PrevioOutputLog, OutputLog, "|Nombre|").
 %Llamada: beginDialog(1,[],0,OutputLog).
 inicioConversacion(Chatbot, InputLog, Hour, StringTime, OutputLog):-
-     (iC1(Chatbot), (Hour >= 6), (Hour < 12), atom_concat(StringTime, 'Chatbot: Buenos Dias, Cual es tu nombre?', String1), agregar(InputLog, OutputLog, String1)),!;
-     (iC1(Chatbot), (Hour >= 12), (Hour < 20), atom_concat(StringTime, 'Chatbot: Buenas Tardes, Cual es tu nombre?', String2), agregar(InputLog, OutputLog, String2)),!;
-     (iC1(Chatbot), (Hour > 20), atom_concat(StringTime, 'Chatbot: Buenas Noches, Cual es tu nombre?', String3), agregar(InputLog, OutputLog, String3)),!;
-     (iC1(Chatbot), (Hour < 6), (Hour >= 0), atom_concat(StringTime, 'Chatbot: Buenas, Cual es tu nombre?', String4), agregar(InputLog, OutputLog, String4)),!;
-     (iC2(Chatbot,Seed), atom_concat(StringTime, 'Chatbot: Hola!, Como te llamai?', String5), agregar(InputLog, OutputLog, String5)),!;
-     (iC3(Chatbot,Seed), atom_concat(StringTime, 'Chatbot: Buena!, Como te llamas?', String6), agregar(InputLog, OutputLog, String6)).
+     (iC1(Chatbot), (Hour >= 6), (Hour < 12), string_concat(StringTime, "Chatbot: Buenos Dias, Cual es tu nombre?", String1), agregar(InputLog, OutputLog, String1)),!;
+     (iC1(Chatbot), (Hour >= 12), (Hour < 20), string_concat(StringTime, "Chatbot: Buenas Tardes, Cual es tu nombre?", String2), agregar(InputLog, OutputLog, String2)),!;
+     (iC1(Chatbot), (Hour > 20), string_concat(StringTime, "Chatbot: Buenas Noches, Cual es tu nombre?", String3), agregar(InputLog, OutputLog, String3)),!;
+     (iC1(Chatbot), (Hour < 6), (Hour >= 0), string_concat(StringTime, "Chatbot: Buenas, Cual es tu nombre?", String4), agregar(InputLog, OutputLog, String4)),!;
+     (iC2(Chatbot,Seed), string_concat(StringTime, "Chatbot: Hola!, Como te llamai?", String5), agregar(InputLog, OutputLog, String5)),!;
+     (iC3(Chatbot,Seed), string_concat(StringTime, "Chatbot: Buena!, Como te llamas?", String6), agregar(InputLog, OutputLog, String6)).
 
+sendMessage(Msg, Chatbot, InputLog, Seed, OutputLog):-
+     getLast(InputLog, OutputLog).
 
 horaYfechaActual(StringTime):-
      get_time(T), stamp_date_time(T, date(Y, MO, D, H, M, S, _, _, _), 'local'),
      S2 is round(S),
-     atom_concat(D,'/', UNO),
-     atom_concat(UNO, MO, DOS),
-     atom_concat(DOS,'/', TRES),
-     atom_concat(TRES, Y, CUATRO),
-     atom_concat(CUATRO, ' ', CINCO),
-     atom_concat(CINCO, H, SEIS),
-     atom_concat(SEIS, ':', SIETE),
-     atom_concat(SIETE, M, OCHO),
-     atom_concat(OCHO, ':', NUEVE),
-     atom_concat(NUEVE, S2, DIEZ),
-     atom_concat(DIEZ, " ", StringTime).
+     string_concat(D,"/", UNO),
+     string_concat(UNO, MO, DOS),
+     string_concat(DOS,"/", TRES),
+     string_concat(TRES, Y, CUATRO),
+     string_concat(CUATRO, " ", CINCO),
+     string_concat(CINCO, H, SEIS),
+     string_concat(SEIS, ":", SIETE),
+     string_concat(SIETE, M, OCHO),
+     string_concat(OCHO, ":", NUEVE),
+     string_concat(NUEVE, S2, DIEZ),
+     string_concat(DIEZ, " ", StringTime).
 
+getLast([], X).
+getLast([X], X).
+getLast([X|Xs], LastElement):-
+     getLast(Xs, LastElement).
 
 logToStr([], X).
 logToStr([X|Xs], X).
